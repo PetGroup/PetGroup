a196394090da0c416823ac30ed38aad6f4802e0d
Tolecen <taox.l@163.com>
2013-08-13 17:40:32 +0800

    Initial Commit

---
+++ PetGroup/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PetGroup
+//
+//  Created by Tolecen on 13-8-13.
+//  Copyright (c) 2013年 Tolecen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
