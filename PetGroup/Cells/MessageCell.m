6a1c50d916de8af81706b091c319052447fa23c5
Tolecen <taox.l@163.com>
2013-08-28 15:41:45 +0800

    信息显示优化

--- PetGroup/Cells/MessageCell.m
+++ PetGroup/Cells/MessageCell.m
@@ -15,20 +15,16 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
-        UIView * bgV = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        [bgV setBackgroundColor:[UIColor clearColor]];
-        [self addSubview:bgV];
-        self.backgroudImageV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        [bgV addSubview:self.backgroudImageV];
+        self.contentView.backgroundColor = [UIColor colorWithRed:0.98 green:0.98 blue:0.98 alpha:1];
         self.headImageV = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 50, 50)];
-        self.headImageV.backgroundColor = [UIColor whiteColor];
+        self.headImageV.backgroundColor = [UIColor clearColor];
         self.headImageV.layer.cornerRadius = 5;
         self.headImageV.layer.masksToBounds=YES;
-        [bgV addSubview:self.headImageV];
+        [self.contentView addSubview:self.headImageV];
         self.notiBgV = [[UIImageView alloc] initWithFrame:CGRectMake(38, 0, 28, 22)];
         [self.notiBgV setImage:[UIImage imageNamed:@"redCB.png"]];
         self.notiBgV.tag=999;
-        [self addSubview:self.notiBgV];
+        [self.contentView addSubview:self.notiBgV];
 //        UILabel * numberLabel = [[UILabel alloc] initWithFrame:CGRectMake(-1, 0, 30, 22)];
 //        [numberLabel setBackgroundColor:[UIColor clearColor]];
 //        [numberLabel setTextColor:[UIColor whiteColor]];
@@ -44,18 +40,21 @@
         self.unreadCountLabel.hidden = YES;
         self.nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(70, 10, 180, 20)];
         [self.nameLabel setTextAlignment:NSTextAlignmentLeft];
+        self.nameLabel.backgroundColor = [UIColor clearColor];
         [self.nameLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:18]];
-        [bgV addSubview:self.nameLabel];
-        self.contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(70, 40, 140, 20)];
+        [self.contentView addSubview:self.nameLabel];
+        self.contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(70, 40, 240, 20)];
+        self.contentLabel.backgroundColor = [UIColor clearColor];
         [self.contentLabel setTextAlignment:NSTextAlignmentLeft];
         [self.contentLabel setTextColor:[UIColor grayColor]];
-        [bgV addSubview:self.contentLabel];
+        [self.contentView addSubview:self.contentLabel];
         self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(210, 10, 100, 20)];
         [self.timeLabel setTextAlignment:NSTextAlignmentRight];
         [self.timeLabel setTextColor:[UIColor grayColor]];
+        self.timeLabel.backgroundColor = [UIColor clearColor];
         [self.timeLabel setFont:[UIFont systemFontOfSize:14]];
         [self.timeLabel setAdjustsFontSizeToFitWidth:YES];
-        [bgV addSubview:self.timeLabel];
+        [self.contentView addSubview:self.timeLabel];
         
     }
     return self;
