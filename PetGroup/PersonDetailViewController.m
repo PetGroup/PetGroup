9c3d607ac6696c553fd5648755aab6c3a664479a
Tolecen <taox.l@163.com>
2013-09-14 14:19:20 +0800

    联系人动态BUG

--- PetGroup/PersonDetailViewController.m
+++ PetGroup/PersonDetailViewController.m
@@ -225,8 +225,10 @@
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     }
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-    
-    [self reloadDynamicData];
+    if (!self.needRequest) {
+        [self reloadDynamicData];
+    }
+
 }
 -(void)cancelBtnDo
 {
@@ -577,7 +579,7 @@
         if (self.myFriend) {
             [DataStoreManager saveUserInfo:recDict];
         }
-     
+        [self reloadDynamicData];
 
     }];
 
