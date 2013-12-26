75c031c93df1e9b120da0d3fc587e51249e8372f
wangxr <wangxr1990123@126.com>
2013-12-26 14:25:05 +0800

    haha

--- PetGroup/DiscoverViewController.m
+++ PetGroup/DiscoverViewController.m
@@ -13,10 +13,13 @@
 #import "PetknowledgeViewController.h"
 #import "TempData.h"
 #import "CustomTabBar.h"
+#import "EGOImageButton.h"
+#import "WebViewViewController.h"
 @interface DiscoverViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic,retain)UITableView* tableV;
 @property (nonatomic,retain)NSArray*nameArray;
 @property (nonatomic,retain)NSArray*iconNameArray;
+@property (nonatomic,retain)NSArray* advertisementArray;
 @end
 
 @implementation DiscoverViewController
@@ -58,6 +61,8 @@
     _tableV.dataSource = self;
     _tableV.backgroundView = nil;
     [self.view addSubview:_tableV];
+    
+    [self loadAdvertisement];
 }
 -(void)viewWillAppear:(BOOL)animated
 {
@@ -73,6 +78,7 @@
         [self.customTabBarController hidesTabBar:NO animated:YES];
         [[TempData sharedInstance] Panned:YES];
     }
+    [self advertisement];
 }
 - (void)didReceiveMemoryWarning
 {
@@ -103,28 +109,14 @@
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *cellIdentifier = @"cell1";
-    MoreCell *cell = (MoreCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if (cell == nil) {
-        cell = [[MoreCell alloc] initWithStyle:UITableViewCellStyleSubtitle  reuseIdentifier:cellIdentifier];
-        cell.selectionStyle = UITableViewCellSelectionStyleGray;
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    }
-
-//    if (cell == nil) {@property (strong,nonatomic) UIImageView * notiBgV;
-//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
-//        cell.selectionStyle = UITableViewCellSelectionStyleGray;
-//        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    if (indexPath.row==4) {
-        cell.headImageV.hidden = YES;
-        cell.titleLabel.hidden = YES;
-        cell.arrow.hidden = YES;
-        cell.contentView.backgroundColor = [UIColor whiteColor];
-        cell.selectionStyle = UITableViewCellSelectionStyleNone;
-        cell.accessoryType = UITableViewCellAccessoryNone;
-        
-    }
-    else{
+    if (indexPath.row!=4) {
+        static NSString *cellIdentifier = @"cell1";
+        MoreCell *cell = (MoreCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (cell == nil) {
+            cell = [[MoreCell alloc] initWithStyle:UITableViewCellStyleSubtitle  reuseIdentifier:cellIdentifier];
+            cell.selectionStyle = UITableViewCellSelectionStyleGray;
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        }
         cell.headImageV.hidden = NO;
         cell.titleLabel.hidden = NO;
         cell.arrow.hidden = NO;
@@ -136,9 +128,17 @@
         [cell.titleLabel setFrame:CGRectMake(70, 20, 100, 20)];
         [cell.arrow setFrame:CGRectMake(287, 18.5, 8.5, 12.5)];
         cell.selectionStyle = UITableViewCellSelectionStyleGray;
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        return cell;
+    }else{
+        static NSString *cellIdentifier = @"advertisementCell";
+        UITableViewCell* cell =(UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle  reuseIdentifier:cellIdentifier];
+            cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        }
+        return cell;
     }
-    return cell;
 }
 #pragma mark - Table view delegate
 
@@ -167,5 +167,47 @@
     }
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     [self.customTabBarController hidesTabBar:YES animated:YES];
+}
+#pragma mark - advertisement
+//生成广告条,如果视图条目增加,增加TableView.from的修改逻辑
+-(void)advertisement
+{
+    if ((![self.view viewWithTag:2013])&&(_advertisementArray.count > 0)) {
+        UIScrollView * sc = [[UIScrollView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height-49-60 , 320, 60)];
+        sc.backgroundColor = [UIColor blackColor];
+        sc.contentSize = CGSizeMake(320*_advertisementArray.count, 60);
+        for (int i = 0;i<_advertisementArray.count;i++) {
+            EGOImageButton * button = [[EGOImageButton alloc]initWithFrame:CGRectMake(i*320, 0, 320, 60)];
+            button.tag = 100+i;
+            button.imageURL = [NSURL URLWithString:_advertisementArray[0]];
+            [button addTarget:self action:@selector(readAdvertisement:) forControlEvents:UIControlEventTouchUpInside];
+            [sc addSubview:button];
+        }
+        sc.tag = 2014;
+        sc.pagingEnabled = YES;
+        [self.view addSubview:sc];
+        UIButton * deleteB  = [UIButton buttonWithType:UIButtonTypeCustom];
+        deleteB.tag = 2013;
+        deleteB.frame = CGRectMake(260, self.view.frame.size.height-49-60, 60, 60);
+        [deleteB setBackgroundImage:[UIImage imageNamed:@"delete"] forState:UIControlStateNormal];
+        [deleteB addTarget:self action:@selector(removeadvertisement) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:deleteB];
+    }
+}
+-(void)removeadvertisement
+{
+    [[self.view viewWithTag:2013] removeFromSuperview];
+    [[self.view viewWithTag:2014] removeFromSuperview];
+}
+-(void)readAdvertisement:(UIButton*)button
+{
+    WebViewViewController* web = [[WebViewViewController alloc]init];
+    web.addressURL = [NSURL URLWithString:_advertisementArray[button.tag-100]];
+    [self presentViewController:web animated:YES completion:nil];
+    
+}
+-(void)loadAdvertisement
+{
+    
 }
 @end
